50f09b7a0e7b0ec5f165b054b5c63633abe9e8e5
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-19 01:38:56 -0600

    added current work for bonus part of Q8

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,4 +1,4 @@
-#1 Create a new file named group_by_exercises.sql
+ #1 Create a new file named group_by_exercises.sql
 
 #2 In your script, use DISTINCT to find the unique titles in the titles table. How many unique titles have there ever been? Answer that in a comment in your SQL file.
 
@@ -54,8 +54,37 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 AND (gender = 'M'
 OR gender = 'F')
-GROUP BY first_name, gender
+GROUP BY first_name, gender;
 
 
 
 #8 Using your query that generates a username for all of the employees, generate a count employees for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames are there?
+
+
+SELECT lower(concat(
+	   substr(first_name,1,1),
+	   substr(last_name,1,4),
+	    "_",
+	   substr(birth_date, 6, 2),
+	   substr(birth_date, 3, 2))
+	    ) AS username, count(*)
+FROM employees
+GROUP BY username
+ORDER BY count(*) DESC;
+
+#below is my code to figure out the bonus. Not working yet but getting there.
+
+SELECT lower(concat(
+	   substr(first_name,1,1),
+	   substr(last_name,1,4),
+	    "_",
+	   substr(birth_date, 6, 2),
+	   substr(birth_date, 3, 2))
+	    ) AS username, count(*)
+FROM employees
+WHERE sum(count) IN (
+	SELECT count(*)
+	FROM employees
+	HAVING username > 1)
+GROUP BY username
+ORDER BY count(*) DESC;
